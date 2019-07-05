4012f736a48a877cb582902c0414521366932547
Sri Krishna Teja.k <skamma@epsvjw.local>
2019-07-05 15:00:31 +0530

    added aiotal cloud modules

---
+++ src/app/firstlogin/firstlogin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FirstloginRoutingModule } from './firstlogin-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FirstloginRoutingModule
+  ]
+})
+export class FirstloginModule { }
