ac37d19f66f6ec6d4f5e21cf9e57c5e2edf6b390
Srikanth <spothina@epsoftinc.com>
2019-08-29 11:21:34 +0530

    http to https for token point

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,6 @@ export interface AiotalAppConfig {
 export const AppConfig: AiotalAppConfig = {
   portfolioSite: 'http://localhost:52538',
   apiendpoint: 'http://10.11.0.82:9091',
-  tokenendpoint: 'http://10.11.0.82:8080',
+  tokenendpoint: 'https://10.11.0.82:8080',
   socialAndWorkLogin: 'https://kube.aiotaldomain.com:9090'
 };
