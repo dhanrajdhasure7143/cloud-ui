3678234a1dfe0656411e985c140da976de450680
Deepak Pilla <deepak.pilla@epsoftinc.com>
2020-11-20 01:16:06 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -23,8 +23,6 @@ export const AppConfig: AiotalAppConfig = {
   proxyTokenendpoint: 'http://10.11.0.112:3001',
   authorizationendpoint: 'http://eiapdemoapi.epsoftinc.com:96/authservices',
   socialAndWorkLogin: 'http://ezbotdevapi.aiotal.in',
-  notificationsendpoint: 'http://alertsdev.epsoftinc.in',
-  Subscriptionendpoint:'http://subscriptiondev.epsoftinc.in',
   productendpoint: 'http://eiapdev.epsoftinc.in',
   socialLoginRedirectURL: 'http://eiapclouddev.epsoftinc.in/#/user',
   notificationsendpoint: 'http://eiapdemoapi.epsoftinc.com:96/alerts',
