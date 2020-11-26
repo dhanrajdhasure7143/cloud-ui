30650eac46873527daf3d4801097c8ab7f77d789
Deepak Pilla <deepak.pilla@epsoftinc.com>
2020-11-26 12:02:41 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,13 +18,13 @@ export interface AiotalAppConfig {
 
 export const AppConfig: AiotalAppConfig = {
   portfolioSite: 'http://10.11.0.82:9098/ang_pages/startforfree.html',
-  apiendpoint: 'http://eiapdemoapi.epsoftinc.com:96/aiotalplatform',
+  apiendpoint: 'https://eiapdemoapi.epsoftinc.com:96/aiotalplatform',
   tokenendpoint: 'https://eiapdemoapi.epsoftinc.com:96/idm',
   proxyTokenendpoint: 'http://10.11.0.112:3001',
-  authorizationendpoint: 'http://eiapdemoapi.epsoftinc.com:96/authservices',
+  authorizationendpoint: 'https://eiapdemoapi.epsoftinc.com:96/authservices',
   socialAndWorkLogin: 'http://ezbotdevapi.aiotal.in',
-  notificationsendpoint: 'http://eiapdemoapi.epsoftinc.com:96/alerts',
-  Subscriptionendpoint:'http://eiapdemoapi.epsoftinc.com:96/subscriptions',
+  notificationsendpoint: 'https://eiapdemoapi.epsoftinc.com:96/alerts',
+  Subscriptionendpoint:'https://eiapdemoapi.epsoftinc.com:96/subscriptions',
   productendpoint: 'http://eiapdemo.epsoftinc.com:86',
   socialLoginRedirectURL: 'http://eiapclouddemo.epsoftinc.com:86/#/user',
   isSecurityManagerEnabled : false
