96bccf56a3a3c92ce71225e7007ebe123bc977dd
Deepak Pilla <deepak.pilla@epsoftinc.com>
2021-04-21 14:00:29 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -40,20 +40,19 @@ export const AppConfig: AiotalAppConfig = {
 
   // QA Urls
    portfolioSite: 'http://10.11.0.82:9098/ang_pages/startforfree.html',
-   apiendpoint: 'http://apdev.epsoftinc.in',
-   tokenendpoint: 'http://idmdev.epsoftinc.in',
-   proxyTokenendpoint: 'http://10.11.0.107:3001',
-   authorizationendpoint: 'http://authdev.epsoftinc.in',
+   apiendpoint: 'http://apqa.epsoftinc.in',
+   tokenendpoint: 'http://idmqa.epsoftinc.in',
+   proxyTokenendpoint: 'http://10.11.0.108:3001',
+   authorizationendpoint: 'http://authqa.epsoftinc.in',
    socialAndWorkLogin: 'http://ezbotdevapi.aiotal.in',
-   notificationsendpoint: 'http://alertsdev.epsoftinc.in',
-   Subscriptionendpoint:'http://subscriptiondev.epsoftinc.in',
-   productendpoint: 'http://eiapdev.epsoftinc.in',
-   socialLoginRedirectURL: 'http://eiapclouddev.epsoftinc.in/#/user',
-   rpaendpoint:"http://rpadev.epsoftinc.in",
-   piendpoint:"http://pidev.epsoftinc.in",
+   notificationsendpoint: 'http://alertsqa.epsoftinc.in',
+   Subscriptionendpoint:'http://subscriptionqa.epsoftinc.in',
+   productendpoint: 'http://eiapqa.epsoftinc.in',
+   socialLoginRedirectURL: 'http://eiapcloudqa.epsoftinc.in/#/user',
+   rpaendpoint:"http://rpaqa.epsoftinc.in",
+   piendpoint:"http://piqa.epsoftinc.in",
    isTwoFactorAuthenticationEnabled: false,
    isSecurityManagerEnabled : true
-
-
+   
 };
 
