0c7f01f96a1cb78d7d3121e2aa83a3009f3042d4
Deepak Pilla <deepak.pilla@epsoftinc.com>
2022-09-24 06:33:30 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -26,20 +26,20 @@ export const AppConfig: AiotalAppConfig = {
   
    // prod
    portfolioSite: 'https://10.11.0.82:9098/ang_pages/startforfree.html',
-   apiendpoint: 'https://ezflow.demo.epsoftinc.com/aiotalplatform',
-   tokenendpoint: 'https://ezidm.demo.epsoftinc.com',
-   proxyTokenendpoint: 'https://ezflow.demo.epsoftinc.com/securitymgr',
-   authorizationendpoint: 'https://ezflow.demo.epsoftinc.com/authservices',
+   apiendpoint: 'https://ezflow.epsoftinc.com/aiotalplatform',
+   tokenendpoint: 'https://ezflow.idm.epsoftinc.com',
+   proxyTokenendpoint: 'https://ezflow.epsoftinc.com/securitymgr',
+   authorizationendpoint: 'https://ezflow.epsoftinc.com/authservices',
    socialAndWorkLogin: 'https://ezbotdevapi.aiotal.in',
-   notificationsendpoint: 'https://ezflow.demo.epsoftinc.com/alerts',
-   Subscriptionendpoint:'https://ezflow.demo.epsoftinc.com/subscriptions',
-   productendpoint: 'https://eziap.demo.epsoftinc.com',
-   newproductendpoint: 'https://eziap.demo.epsoftinc.com',
-   socialLoginRedirectURL: 'https://ezflow.demo.epsoftinc.com/#/user',
+   notificationsendpoint: 'https://ezflow.epsoftinc.com/alerts',
+   Subscriptionendpoint:'https://ezflow.epsoftinc.com/subscriptions',
+   productendpoint: 'https://ezflow.iap.epsoftinc.com',
+   newproductendpoint: 'https://ezflow.iap.epsoftinc.com',
+   socialLoginRedirectURL: 'https://ezflow.epsoftinc.com/#/user',
    isSecurityManagerEnabled : false,
    isTwoFactorAuthenticationEnabled: false,
-   rpaendpoint:"https://ezflow.demo.epsoftinc.com/wfrpa",
-   piendpoint:"https://ezflow.demo.epsoftinc.com/prcintel",
+   rpaendpoint:"https://ezflow.epsoftinc.com/wfrpa",
+   piendpoint:"https://ezflow.epsoftinc.com/prcintel",
    isNewDesignEnabled : true
   
    // demo
