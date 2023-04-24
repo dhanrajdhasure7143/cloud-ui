9fb830b123a2110025334693e2497ba3d11ef145
ranjith.sigiri <ranjith.sigiri@epsoftinc.com>
2023-04-24 14:45:23 +0530

    staging properties added

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,18 @@
+export const environment = {
+   portfolioSite: 'https://10.11.0.82:9098/ang_pages/startforfree.html',
+   apiendpoint: 'https://ezflow.staging.epsoftinc.com/aiotalplatform',
+   tokenendpoint: 'https://ezidm.staging.epsoftinc.com',
+   proxyTokenendpoint: 'https://ezflow.staging.epsoftinc.com/securitymgr',
+   authorizationendpoint: 'https://ezflow.staging.epsoftinc.com/authservices',
+   socialAndWorkLogin: 'https://ezbotdevapi.aiotal.in',
+   notificationsendpoint: 'https://ezflow.staging.epsoftinc.com/alerts',
+   Subscriptionendpoint:'https://ezflow.staging.epsoftinc.com/subscriptions',
+   productendpoint: 'https://eziap.staging.epsoftinc.com',
+   newproductendpoint: 'https://eziap.staging.epsoftinc.com',
+   socialLoginRedirectURL: 'https://ezflow.staging.epsoftinc.com/#/user',
+   isSecurityManagerEnabled : false,
+   isTwoFactorAuthenticationEnabled: false,
+   rpaendpoint:"https://ezflow.staging.epsoftinc.com/wfrpa",
+   piendpoint:"https://ezflow.staging.epsoftinc.com/prcintel",
+   isNewDesignEnabled : true
+}
